f4607ac3d9d6d4e46120cdedd2d320536a261080
Link4Electronics <link4electronics@gmail.com>
2026-02-07 08:10:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,21 +3,21 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q ymir | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q ymir-emu-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-#export ICON=PATH_OR_URL_TO_ICON
-#export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/pixmaps/ymir-emu.png
+export DESKTOP=/usr/share/applications/ymir-emu.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/ymir
+quick-sharun /usr/bin/ymir-emu
 
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
-quick-sharun --make-appimage
+#quick-sharun --make-appimage
