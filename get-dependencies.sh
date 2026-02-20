626df08b1e126755889134a43d2388a54029cbb7
Link4Electronics <link4electronics@gmail.com>
2026-02-20 17:22:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,18 +7,43 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    fmt           \
-    cereal        \
-    cxxopts       \
-    chrono-date   \
+    cmake         \
     pipewire-jack \
-    rtmidi
+    sdl3          \
+    vcpkg
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package ymir-emu-git
+#make-aur-package 
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of Open Horizon..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/StrikerX3/Ymir"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./Ymir
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./Ymir
+mkdir -p build && cd build
+local cmake_options=(
+        -D Ymir_ENABLE_TESTS=OFF
+        -D Ymir_ENABLE_DEVLOG=OFF
+        -D Ymir_ENABLE_IMGUI_DEMO=OFF
+        -D Ymir_ENABLE_SANDBOX=OFF
+        -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+        --fresh
+    )
+    # Enable AVX2 only for x86_64
+    if [ "$CARCH" == "x86_64" ]; then
+        cmake_options+=(-D Ymir_AVX2=ON)
+    else
+        cmake_options+=(-D Ymir_AVX2=OFF)
+    fi
+cmake .. "${cmake_options[@]}"
+make -j$(nproc)
+mv - ymir-sdl3 ../../AppDir/bin
