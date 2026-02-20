005f70890f98dfa5d27a73b151f1802439f66b41
Link4Electronics <link4electronics@gmail.com>
2026-02-20 17:42:29 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -29,21 +29,23 @@ echo "$VERSION" > ~/version
 mkdir -p ./AppDir/bin
 cd ./Ymir
 mkdir -p build && cd build
-local cmake_options=(
-        -D Ymir_ENABLE_TESTS=OFF
-        -D Ymir_ENABLE_DEVLOG=OFF
-        -D Ymir_ENABLE_IMGUI_DEMO=OFF
-        -D Ymir_ENABLE_SANDBOX=OFF
-        -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
-        --fresh
-    )
-    # Enable AVX2 only for x86_64
-    if [ "$CARCH" == "x86_64" ]; then
-        cmake_options+=(-D Ymir_AVX2=ON)
-    else
-        cmake_options+=(-D Ymir_AVX2=OFF)
-    fi
-cmake .. "${cmake_options[@]}"
+cmake_opts=(
+    -D Ymir_ENABLE_TESTS=OFF
+    -D Ymir_ENABLE_DEVLOG=OFF
+    -D Ymir_ENABLE_IMGUI_DEMO=OFF
+    -D Ymir_ENABLE_SANDBOX=OFF
+    -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+    --fresh
+)
+
+# Enable AVX2 only for x86_64
+if [ "$CARCH" == "x86_64" ]; then
+    cmake_opts+=(-D Ymir_AVX2=ON)
+else
+    cmake_opts+=(-D Ymir_AVX2=OFF)
+fi
+
+cmake .. "${cmake_opts[@]}"
 make -j$(nproc)
 mv -v apps/ymir-sdl3/ymir-sdl3-0.3.0 ../../AppDir/bin/ymir-sdl3
 mv -v ../apps/ymir-sdl3/res/ymir.png ../../AppDir
