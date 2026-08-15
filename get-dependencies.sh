3bf8fabe12afef4a38fe1eb8ec1ade7e5a1fed3d
Link4Electronics <link4electronics@gmail.com>
2026-08-15 20:54:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,10 +18,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package 
-
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of Ymir..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/StrikerX3/Ymir"
