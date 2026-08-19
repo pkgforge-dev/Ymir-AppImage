201ae5e43374656b81268e314a96ad0976c9b975
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:26:01 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,7 +21,7 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-echo "Making nightly build of Ymir..."
+echo "Building Ymir..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/StrikerX3/Ymir"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
