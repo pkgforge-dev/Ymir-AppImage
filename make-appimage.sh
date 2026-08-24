d14b838cd99e2bf970fb3b0a0f21b63c12cfaa76
Link4Electronics <link4electronics@gmail.com>
2026-08-24 09:20:16 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,6 +7,9 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook:wayland-is-broken.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/ymir-emu/Ymir/refs/heads/main/apps/ymir-sdl3/res/ymir.png
+export DESKTOP=https://raw.githubusercontent.com/ymir-emu/Ymir/refs/heads/main/apps/ymir-sdl3/res/io.github.strikerx3.ymir.desktop
+export STARTUPWMCLASS=ymir-sdl3
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
