46bbe7a3f3d5cec5a6d4ed22ce725ca253e4a0b2
Link4Electronics <link4electronics@gmail.com>
2026-08-25 10:22:15 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -18,7 +18,3 @@ quick-sharun ./AppDir/bin/ymir-sdl3
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
-
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --simple-test ./dist/*.AppImage
